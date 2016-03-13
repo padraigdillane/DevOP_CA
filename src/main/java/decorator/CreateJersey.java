452dd7fa278fd463d5995e666b0901626991773e
padraigdillane <padraigdillane@gmail.com>
2016-03-13 14:52:27 +0000

    test

--- src/main/java/decorator/CreateJersey.java
+++ src/main/java/decorator/CreateJersey.java
@@ -2,7 +2,7 @@ package decorator;
 
 public class CreateJersey {
     public static void main(String[] args) {
-        
+        //commit
         Jersey short_sleeve = new NumberPrinting(new NamePrinting(new ShortSleeve()));
         Jersey long_sleeve = new NumberPrinting(new LongSleeve());
         Jersey plain_short = new ShortSleeve();
