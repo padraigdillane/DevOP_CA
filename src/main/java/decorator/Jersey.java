1133e0bf0f736afe87ebfed7ade34e0ff779ac7e
padraigdillane <padraigdillane@gmail.com>
2016-04-05 15:47:32 +0100

    test heroku

--- src/main/java/decorator/Jersey.java
+++ src/main/java/decorator/Jersey.java
@@ -4,7 +4,7 @@ package decorator;
  * Created by Padraig on 10/6/2015.
  */
 public interface Jersey {
-
+//test commit to trigger heroku deploy
     public String createJersey();
     public double cost();
 }
