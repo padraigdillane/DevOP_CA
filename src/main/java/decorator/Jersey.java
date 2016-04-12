6abff0707a09862fea34791347eb71ec1a403f5d
padraigdillane <padraigdillane@gmail.com>
2016-04-12 16:09:46 +0100

    email configuration

--- src/main/java/decorator/Jersey.java
+++ src/main/java/decorator/Jersey.java
@@ -4,7 +4,7 @@ package decorator;
  * Created by Padraig on 10/6/2015.
  */
 public interface Jersey {
-
+    //comment to test email notification
     public String createJersey();
     public double cost();
 }
