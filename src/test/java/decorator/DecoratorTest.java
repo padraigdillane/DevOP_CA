870dd109421f416ef3fdbd749cc4523778857c55
padraigdillane <padraigdillane@gmail.com>
2016-04-19 15:55:08 +0100

    added new test using mocks

--- src/test/java/decorator/DecoratorTest.java
+++ src/test/java/decorator/DecoratorTest.java
@@ -2,13 +2,30 @@ package decorator;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
 /**
  * Created by User on 13/03/2016.
  */
 public class DecoratorTest {
+
+    @Test
+    public void testMOCKLongSleeveWithNumberCost() throws Exception {
+
+        LongSleeve longSleeveMock = mock(LongSleeve.class);
+        when(longSleeveMock.cost()).thenReturn(Double.valueOf(60f));
+
+        Jersey jersey = new NumberPrinting(longSleeveMock);
+        Jersey jersey1 = new NumberPrinting(new LongSleeve());
+
+        double expected = 65.00f;
+        double actual = jersey.cost();
+        double actual1 = jersey1.cost();
+
+        Assert.assertEquals(expected, actual, 0.01f);
+        Assert.assertEquals(expected, actual1, 0.01f);
+    }
 
     @Test
     public void testPlainShortJerseyCost() throws Exception {
